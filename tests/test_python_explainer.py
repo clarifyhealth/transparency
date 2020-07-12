c1d3c8147333a72fea6f18b66d42171ef69c3218
Iman Haji-Abolhassani <imanhajia@bitbucket.org>
2020-07-12 03:51:01 -0700

    Ensemble tree prediction explanation for Scikit-Learn using Pandas and Numpy

---
+++ tests/test_python_explainer.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+from sklearn.datasets import load_diabetes
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score as r2
+from transparency.python.explainer.tree import EnsembleTreeExplainTransformer
+
+def test_python_explainer():
+    # loading the diabetes dataset
+    columns = 'age sex bmi map tc ldl hdl tch ltg glu'.split()
+    diabetes = load_diabetes()
+    X = np.array(pd.DataFrame(diabetes.data, columns=columns))
+    y = diabetes.target
+
+    # train-test-split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+    # model training
+    rf_model = RandomForestRegressor().fit(X_train, y_train)
+    y_pred = rf_model.predict(X_test)
+
+    # regression evaluation: r2 score
+    r2_eval = r2(y_test, y_pred)
+    print(r2_eval)
+
+    # prediction explanation generation
+    expl = EnsembleTreeExplainTransformer(rf_model)
+    contributions, contrib_intercept = expl.predict(X_test)
+
+    average_contribs = zip(columns, np.mean(contributions, axis=0))
+    print(list(average_contribs))
+
+    assert (((np.sum(contributions, axis=1) + contrib_intercept) - y_pred < .01).all())
+
+    return
+
+
---
+++ transparency/python/explainer/tree.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class EnsembleTreeExplainTransformer:
+    def __init__(self, estimator):
+        self.estimator = estimator
+        self.feature_importance_ranks = np.argsort(estimator.feature_importances_)[::-1]
+        self.feature_count = len(self.feature_importance_ranks)
+    def _path_generator(self, X):
+        nf = self.feature_count
+        tree_paths = []
+        for row in X:
+            nzid = np.nonzero(row)[0]
+            tree_paths.append([0] * nf)
+            for id in nzid:
+                tree_paths.append(tree_paths[-1][:id] + [row[id]] + tree_paths[-1][id + 1:])
+        tree_paths = np.array(tree_paths)
+        return tree_paths
+    def predict(self, X):
+        nf = self.feature_count
+        tree_paths = self._path_generator(np.array(X))
+        path_outcomes = self.estimator.predict(tree_paths)
+        incremental_contributions = path_outcomes[1:] - path_outcomes[:-1]
+        contributions = []
+        current_contribs = [0] * nf
+        for path_no, path in enumerate(tree_paths[1:]):
+            if (path == 0).all():
+                contributions.append(current_contribs)
+                current_contribs = [0] * nf
+            else:
+                current_contribs[np.argmax(np.nonzero(path)[0])] = incremental_contributions[path_no]
+        contributions.append(current_contribs)
+        contributions = np.array(contributions)[:, np.argsort(self.feature_importance_ranks)]
+        contrib_intercept = path_outcomes[0]
+        return contributions, contrib_intercept
+    def fit(self):
+        return self
