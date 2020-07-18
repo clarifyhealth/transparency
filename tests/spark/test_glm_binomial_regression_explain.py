81827f1bcc51da9e9cbb28e8f3d351083b9cdb93
ahenrick <share.code@aol.com>
2020-07-18 12:52:03 -0700

    add binomial logit test

--- tests/spark/test_glm_binomial_regression_explain.py
+++ tests/spark/test_glm_binomial_regression_explain.py
@@ -10,18 +10,16 @@ from testutil.common import get_glm_pipeline_stages, get_feature_coefficients, g
 
 
 @pytest.mark.parametrize("family", ["tweedie"])
-@pytest.mark.parametrize("variance_power", [0.0, 1.0, 1.1, 1.2, 2.0])
-def test_explain_regressor(spark_session: SparkSession, family: str, variance_power: float):
+@pytest.mark.parametrize("link", ["logit"])
+def test_explain_regressor(spark_session: SparkSession, family: str, link: str):
     data_dir = Path(__file__).parent.parent.joinpath('data')
 
-    link_power = 1 - variance_power
+    prima_indian_diabetes_csv: Path = data_dir / 'classification' / 'dataset_prima_indian_diabetes.csv'
 
-    boston_csv: Path = data_dir / 'regression' / 'dataset_boston.csv'
+    prima_indian_diabetes_df: DataFrame = spark_session.read.option("header", True).option("inferSchema", True).csv(
+        prima_indian_diabetes_csv.as_uri())
 
-    boston_df: DataFrame = spark_session.read.option("header", True).option("inferSchema", True).csv(
-        boston_csv.as_uri())
-
-    label_column = 'medv'
+    label_column = 'outcome'
     features_column = f"features_{label_column}"
     prediction_column = f"prediction_{label_column}"
 
@@ -33,14 +31,13 @@ def test_explain_regressor(spark_session: SparkSession, family: str, variance_po
     predictions_view = f"predictions_{label_column}_view"
 
     categorical_columns = []
-    continuous_columns = [x for x in boston_df.columns if x not in ['id', label_column]]
+    continuous_columns = [x for x in prima_indian_diabetes_df.columns if x not in ['id', label_column]]
 
-    stages = get_glm_pipeline_stages(categorical_columns, continuous_columns, label_column, family=family,
-                                     variance_power=variance_power, link_power=link_power)
+    stages = get_glm_pipeline_stages(categorical_columns, continuous_columns, label_column, family=family, link=link)
 
     pipeline = Pipeline(stages=stages)
 
-    pipeline_model: PipelineModel = pipeline.fit(boston_df)
+    pipeline_model: PipelineModel = pipeline.fit(prima_indian_diabetes_df)
     glm_model = pipeline_model.stages[-1]
     string_id = uuid.uuid4()
     glm_model_path: str = f"/tmp/{string_id}"
@@ -48,7 +45,7 @@ def test_explain_regressor(spark_session: SparkSession, family: str, variance_po
 
     # param_dict = extract_params(glm_model)
 
-    prediction_df = pipeline_model.transform(boston_df)
+    prediction_df = pipeline_model.transform(prima_indian_diabetes_df)
     prediction_df.createOrReplaceTempView(predictions_view)
 
     prediction_df.show(truncate=False)
@@ -59,7 +56,7 @@ def test_explain_regressor(spark_session: SparkSession, family: str, variance_po
     features_coefficients_df.show(truncate=False)
 
     explain_stages = get_glm_explain_stages(predictions_view, features_coefficient_view, label_column,
-                                            family=family, variance_power=variance_power, link_power=link_power)
+                                            family=family, link=link)
 
     explain_pipeline = Pipeline(stages=explain_stages)
 
